379d6f501adc746498cdcb2ce9ed262ba6c89ed6
K_D_A <Dima1234567890000@yandex.ru>
2024-05-10 17:30:03 +0400

    spring_jdb исправление ошибок.

--- spring_jdbc/src/main/java/com/example/spring_jdbc/controllers/ClientController.java
+++ spring_jdbc/src/main/java/com/example/spring_jdbc/controllers/ClientController.java
@@ -1,28 +1,33 @@
 package com.example.spring_jdbc.controllers;
 
 import com.example.spring_jdbc.model.Client;
+import com.example.spring_jdbc.repositories.ClientRepository;
 import com.example.spring_jdbc.services.ClientService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
 public class ClientController {
     private final ClientService clientService;
+    @Autowired
+    private final ClientRepository clientRepository;
 
-    @GetMapping("/")
-    public String client(@RequestParam(name = "name", required = false) String name, Model model) {
-        model.addAttribute("client", clientService.listClients(name));
-        return "name";
+    @RequestMapping("/")
+    public List<Client> getClient() {
+        return clientRepository.findAll();
     }
+//    public String client(@RequestParam(name = "name", required = false) String name) {
+//        clientService.listClients(name);
+//        return "name";
+//    }
 
-    @GetMapping("/client/{id}")
-    public String clientInfo(@PathVariable Long id, Model model) {
-        model.addAttribute("client", clientService.getClientById(id));
+    @RequestMapping("/client/{id}")
+    public String clientInfo(@PathVariable Long id) {
+        clientService.getClientById(id);
         return "client-info";
     }
 
--- spring_jdbc/src/main/java/com/example/spring_jdbc/repositories/ClientRepository.java
+++ spring_jdbc/src/main/java/com/example/spring_jdbc/repositories/ClientRepository.java
@@ -2,9 +2,12 @@ package com.example.spring_jdbc.repositories;
 
 import com.example.spring_jdbc.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    List<Client> findByName(String title);
+    List<Client> findByName(String name);
 }
